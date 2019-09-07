b97db158035907a2d9b4392346e5f2bd53acf587
Sumanth <sumanth9571@gmail.com>
2019-09-07 18:41:52 -0400

    Recipe Book Items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import {RecipedetailComponent} from './recipe-list/recipe-detail/recipedetail.component';
-import {RecipeItemComponent} from './recipe-list/recipe-item/recipe-item.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
@@ -15,10 +16,11 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     AppComponent,
     HeaderComponent,
     RecipeListComponent,
-    RecipedetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
   ],
   imports: [
     BrowserModule
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
 
+  collapsed = true;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
